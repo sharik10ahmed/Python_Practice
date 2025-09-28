e6eb9ea0f8a6a4c143b772af25483c7ef2d10b29
sharik10ahmed <sharikahmed757@gmail.com>
2025-09-28 19:01:34 +0530

    first commit

---
+++ into.py
@@ -0,0 +1,9 @@
+# print("hello Wilmont")
+# some = 2
+# print(some)
+# name = input("What is your name? ")
+# print("Hello, " + name + "!")
+base=2
+exponent=3
+result = pow(base,exponent)  # This calculates 2 to the power of 3
+print(2**3)
